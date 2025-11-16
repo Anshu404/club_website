19234884c4c5f3c53c3d44b59fa98932693064a6
Anshu Kumar Mandal <anshukumarmandal@Anshus-MacBook-Air.local>
2025-11-16 21:12:09 +0530

    fix: disable react/no-unescaped-entities

--- types/blog.ts
+++ types/blog.ts
@@ -1,19 +1,16 @@
-// Naya 'ImageGallery' type
 type ImageGallery = {
-  folder: string; // e.g., "bridge-building-contest"
-  count: number;  // Total number of images in the folder
-  fileType: "jpg" | "png"; // The type of images
+  folder: string;
+  count: number;
+  fileType: "jpg" | "png";
 };
 
-// This is the updated Author type
 export type Author = {
   name: string;
-  imageLight: string; // The logo for day mode
-  imageDark: string;  // The logo for night mode
+  imageLight: string;
+  imageDark: string;
   designation: string;
 };
 
-// This Blog type now correctly uses the exported Author type
 export type Blog = {
   slug: string;
   title: string;
